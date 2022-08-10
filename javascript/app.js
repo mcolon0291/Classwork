9accee4d060452857bab498eb8ce86b3628b2e35
Marleni Colon <mcolon0291@gmail.com>
2022-08-10 16:49:43 -0400

    variable homework

---
+++ javascript/app.js
@@ -0,0 +1,164 @@
+
+// const age=10;
+
+// //string
+// const firstname= "Marleni";
+// const lastname= "Colon";
+
+
+// console.log(firstname)
+
+// //CAMEL CASE
+// // myFavoriteDesert
+
+// //PASCEL CASE
+// //MyFavoriteDesert
+
+// //KEBAB CASE
+// //my-favorite-desert
+
+// //LOGICAL OPERATORS
+// //AND &&
+// //OR  ||
+// //NOT !
+
+// //SHORTHANDS
+
+// let fire = 0; //0
+
+// fire-= 10 //-10
+
+// //Only for incrementing or decrementing by 1
+// fire-- // decrementing by 1 // -11
+// fire ++ // incrementing by 1 // -10
+
+// fire = fire - 10 // -20
+
+// console.log(fire)
+
+
+// // Loose Comparsion
+// // === (3 equal signs)
+
+// // Loose Comparsion
+// // == (2 equal signs)
+
+// // Assignment Operator
+// // = (1 equal sign)
+
+// let x = 6;
+// let y = 3;
+
+
+// let num1 = 2;
+// let num2 = 4;
+// let result = num1 + num2 
+// let num3 = 6;
+// console.log(result)
+
+// total = num1 + num2 + num3; */
+// // 12 =  2 + 4 + 6 */
+
+
+
+// let age = 19
+// if(age > = 18) {
+// console.log( "welcome to the site")
+// }
+// else{
+//     console.log("access denied. You must be 18+ to enter this site")
+// }
+// }
+
+
+
+// let pass = true;
+// let grade = 67;
+
+// if (pass) {
+//     if (grade >= 90) {
+//         console.log('A');
+//     } else if (grade >= 80) {
+//         console.log('B');
+//     } else if (grade >= 70) {
+//         console.log('C');
+//     } else {
+//         console.log('D');
+//     }
+// } else {
+//     console.log('F');
+// }
+
+// if (true {
+
+//     let blockScoped = 'block'
+
+//     // inner (nested)
+//     if (true) {
+
+//     }
+// })
+
+// let number = 1; 
+
+// switch (number) {
+//     case 1:
+//         console.log ('inside of case 1')
+//         break;
+//     case 2: 
+//         console.log ('inside of case 2')
+//         break; 
+//     case 3: 
+//          console.log ('inside of case 3')
+//          break;
+//     default:
+//         console.log ('do something else')
+//         break;
+// }
+
+// // ternary operator (three parts)
+
+// let lighting = 'light'
+
+// let dayorNight=
+// lighting == 'dark' ? 
+// 'night time' :
+// lighting === 'cloudy' ? 
+//  'overcast'
+//  'day time' 
+
+/* 
+
+let lighting = 'light
+let day0rnight;
+
+if (lighting === 'dark'){
+    day0rNight = 'night time'
+} else {
+    dayorNight = 'overcast'
+} else {
+    dayorNight = 'day time'
+}
+*/
+
+// let letter = "B"
+
+// switch(letter){
+//     case "A":
+//     case "E":
+//     case "I":
+//     case "O":
+//     case "U":
+//         console.log(letter + " is a vowel")
+//     break;
+//     default: 
+//         console.log ("this letter is a constonant") 
+//    }
+ 
+
+// Exercise 2 - Terenayr Operator
+
+   let number = 3; 
+
+   oddEven = number % 2 === 0 ? "It is even" : "It is odd";
+   console.log(oddEven);
