96c383ac2579a1d9e7f9da5a36f1af4e0e835419
Marleni Colon <mcolon0291@gmail.com>
2022-08-18 15:19:12 -0400

    ReviewJS

---
+++ Review/review.js
@@ -0,0 +1,21 @@
+//I. variables& Datatypes//
+//a1- The equal will assign a value to a variable. ex- (let B = 22)
+//a2- Will need to be resigned with a new value.
+//a3- We will combine each variable to new variable. 
+//a4- Declare , Assign , Define ex -( let X = 22) value
+//a5- pseudcoding -writing out the syntax or explaination of the problem. It helps you set up the syntax. 
+//a6- 70/30 percent should be spent.
+
+//b1-b2- const firstVariable = 'Hello World'
+//b3- let firstVariable = 22
+//b4- let firstVariable = secondVariable
+//b5- let secondVariable = 'hey'
+//b6- hey
+// let secondVariable = 'hey';
+// let firstVariable = secondVariable;
+// console.log(firstVariable)
+//b7- 
+// let yourName = 'Marleni'
+// console.log('Hello, my name is ' + yourName)
+
+//c1- 
