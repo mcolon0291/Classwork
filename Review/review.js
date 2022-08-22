a0b27d6762dc1380c6096300101bb3699abed753
Marleni Colon <mcolon0291@gmail.com>
2022-08-22 15:57:30 -0400

    Review

--- Review/review.js
+++ Review/review.js
@@ -45,11 +45,96 @@
 //     console.log("Hey! You're not a cow.")
 //  }
 
-//E1- 
-let age = 10
-if (age >= 16 ){
-console.log ('Here are the keys!')
-}
-else if (age < 16){
-    console.log("Sorry, you're too young.")
-}
+// //E1- E2 
+// let age = 10
+// if (age >= 16 ){
+// console.log ('Here are the keys!')
+// }
+// else if (age < 16){
+//     console.log("Sorry, you're too young.")
+// }
+
+//Loops 
+// // A1- 
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// A2-
+// for (let i = 10; i <= 400; i++){
+//     console.log(i)
+// }
+// A3- 
+// for(let i = 12; i <= 4000; i+=3){
+// console.log(i)
+// }
+
+//B1-B2 
+// for(let i = 1; i <= 100; i++ ){
+//     if (i % 2 === 0){
+//     console.log(i + '<-- is an even number')
+//     }
+//     else{
+//     console.log(i)
+//     }
+// }
+
+// C1- C3
+// for(let i = 1; i <= 100; i++){
+//     if (i % 5 === 0 && i % 3 === 0){
+//         console.log("I found a " + i + " High five!")
+//         console.log("I found a " + i + " Three is a crowd.")
+//     }
+//     else if (i % 5 === 0){
+//         console.log("I found a " + i + " High five!")
+
+//     }
+//     else if(i % 3 === 0){
+//         console.log("I found a " + i + " Three is a crowd.")
+
+//     }
+  
+// }
+
+//D1- 
+// let bank_Account = () => {
+//     let sum= 0
+//     for (let i = 0; i <= 10; i++){
+//     sum += i
+// }
+// return sum
+
+// }
+// console.log(bank_Account())
+
+// let bank_Account = () => {
+// let sum = 0
+// for (let i = 0; i <= 100; i++){
+//     sum += i 
+// }
+// return sum * 2
+// }
+// console.log(bank_Account())
+
+// //Arrays & Control flow
+// A1- Elements 
+// A2- Yes, the order that you create.
+//A3- Shopping list, grocery list
+
+// B1- 
+// let quotes = [ 'Be yourself', 'Be practical', 'Love yourself']
+
+// C1- randomThings[0]
+// C2-C3 
+// const randomThings = [1, 10, 'Hello', true]
+// randomThings[2] = 'World'
+// console.log(randomThings)
+
+const ourClass = ['Slaty', 'Zoom', 'Sardine', 'Slack', 'Github']
+// D1- ourClass[3]
+// D2- 
+// ourClass[4] = 'Octocat'
+// console.log(ourClass)
+
+// D3-
+ ourClass.push('Cloud City')
+ console.log(ourClass)
